70287c615b4fb98fce3b9de47e6d7168d2155263
mzeinali <m@zmim.ir>
2023-06-05 02:14:53 +0330

    add unicode list script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-course
 Python scripts for my python learning course!!!
 1. `rock_paper_scissors`: You know the game 😀
+2. `unicode_list`: A `python` script to list all unicode characrters 📄
---
+++ unicode_list/main.py
@@ -0,0 +1,14 @@
+# A python script to list all unicode characters
+import sys
+import unicodedata
+txtfile = "unicode_table.txt"
+print("creating file: " + txtfile)
+with open(txtfile, "w", encoding="utf-8", errors='ignore') as file:
+    for uc in range(sys.maxunicode):
+        try:
+            line = f"{str(uc)}, {chr(uc)}, \
+{unicodedata.name(chr(uc)).lower()}, \
+{unicodedata.category(chr(uc)).lower()}, {hex(uc)}\n"
+        except ValueError:
+            line = f"{str(uc)}, {chr(uc)}, none, none, {hex(uc)}"
+        file.write(line)
